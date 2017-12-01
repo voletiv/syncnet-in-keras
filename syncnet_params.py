b75a44547fca3f0c40f289e796e0b3d89c25c9f4
Vikram Voleti <vikky2904@gmail.com>
2017-12-02 01:14:22 +0530

    Pre-pended SYNCNET_DIR to paths

--- syncnet_params.py
+++ syncnet_params.py
@@ -1,12 +1,18 @@
+import os
+
 # SYNCNET - PARAMS
 
 #############################################################
 # PARAMS
 #############################################################
 
-SYNCNET_WEIGHTS_FILE_V4 = 'syncnet-weights/lipsync_v4_73.mat'
+# SYNCNET directory
+if 'SYNCNET_DIR' not in dir():
+    SYNCNET_DIR = os.path.dirname(os.path.realpath(__file__))
 
-SYNCNET_WEIGHTS_FILE_V7 = 'syncnet-weights/lipsync_v7_73.mat'
+SYNCNET_WEIGHTS_FILE_V4 = os.path.join(SYNCNET_DIR, 'syncnet-weights/lipsync_v4_73.mat')
+
+SYNCNET_WEIGHTS_FILE_V7 = os.path.join(SYNCNET_DIR, 'syncnet-weights/lipsync_v7_73.mat')
 
 # IMAGE_DATA FORMAT = {'channels_first', 'channels_last'}
 IMAGE_DATA_FORMAT = 'channels_last'
